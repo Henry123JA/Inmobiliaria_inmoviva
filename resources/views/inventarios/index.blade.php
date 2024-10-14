1e7eb54c98c5f693319f69e5c3d224ebe8451f04
Limberg03 <limberg.ent4@gmail.com>
2024-10-14 10:06:00 -0400

    limberg1

--- resources/views/inventarios/index.blade.php
+++ resources/views/inventarios/index.blade.php
@@ -3,7 +3,7 @@
 
         <x-slot name="header">
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Lista de Propiedades (Inventario)
+                Lista de Propiedades ( Inventario )
             </h2>
         </x-slot>
         <div>
